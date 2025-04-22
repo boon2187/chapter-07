b1410cb06f7885d4e6d371313a8db4b7eb5b8a2c
boon2187 <fumiya.mai.ito@gmail.com>
2025-04-22 17:48:27 +0900

    APIから投稿データを読み込んで保存するステートと、ロード中のステートを作成。型としてPostをインポート。

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
+import { Post } from "../types/post";
 
 export default function Home() {
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
   return <div>Home</div>;
 }
