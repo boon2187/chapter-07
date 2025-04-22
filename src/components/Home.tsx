654d0fd9f2ac5d3c5bf41139bc4b4b490d0babfb
boon2187 <fumiya.mai.ito@gmail.com>
2025-04-22 18:03:56 +0900

    APIから投稿データを取得して、とりあえず表示するまで。

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,8 +1,40 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Post } from "../types/post";
 
 export default function Home() {
   const [posts, setPosts] = useState<Post[]>([]);
-  const [loading, setLoading] = useState<boolean>(true);
-  return <div>Home</div>;
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  // Postデータを取得
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch(
+          "https://1hmfpsvto6.execute-api.ap-northeast-1.amazonaws.com/dev/posts"
+        );
+        const data = await response.json();
+        setPosts(data.posts);
+        // console.log("取得したデータ:", data.posts);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchPosts();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="h-screen flex items-center justify-center">
+        <p className="text-lg">読み込み中...</p>
+      </div>
+    );
+  }
+  return (
+    <div>
+      {posts.map((post) => (
+        <div key={post.id}>{post.title}</div>
+      ))}
+    </div>
+  );
 }
