7415968e160960fad262d0fd826b0f04e6d82782
boon2187 <fumiya.mai.ito@gmail.com>
2025-04-23 21:55:23 +0900

    型の定義を一つにまとめた

--- src/types/date.ts
+++
@@ -1,2 +0,0 @@
-// 日付を表す型
-export type DateType = Date | string;
--- src/types/form.ts
+++
@@ -1,5 +0,0 @@
-import { z } from "zod";
-import { contactFormSchema } from "../schemas/contactForm";
-
-// スキーマから型を推論
-export type ContactFormData = z.infer<typeof contactFormSchema>;
